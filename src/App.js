bd13cf907c6ab319fbc424b127e35a0f5cb69e84
susnitdgp <susnitdgp@gmail.com>
2020-09-30 00:07:19 +0530

    2 files modified, 11 deleted

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
-import React from "react";
+import React , {Component} from "react";
 import "./style.css";
-import "antd/dist/antd.css";
-
-import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
-
-const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
-
 import { BrowserRouter  } from 'react-router-dom';
 
 
-export default function App() {
+class App extends Component{
 
-  state = {
-    current: 'mail',
-  };
+  render(){
 
-  handleClick = e => {
-    console.log('click ', e);
-    this.setState({ current: e.key });
-  };
-  return (
+     return (
 
     
      <BrowserRouter>
-      
-    <Layout className="layout">
-    <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-          Navigation Two
-        </Menu.Item>
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
-      </Menu>
-    
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content">Content</div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-  </Layout>
-        
 
+     <h1>Hello</h1>
+      
+    
      </BrowserRouter>
   );
+
+  }
+ 
 }
+
+export default App
