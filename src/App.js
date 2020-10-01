1aa629b406d881caf8652a38b06a6d142b4954d3
susnitdgp <susnitdgp@gmail.com>
2020-10-01 12:59:12 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -11,12 +11,8 @@ import HomePage from './Components/Home'
 
 import { withAuthentication } from './Components/Session';
 import { Divider } from 'antd';
-import { Steps,Layout, Menu, Breadcrumb } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined} from '@ant-design/icons';
 
-const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
-const { Step } = Steps;
+
 
 class App extends Component{
 
@@ -56,54 +52,6 @@ class App extends Component{
 
      return (
      <BrowserRouter>
-
-
-     <Layout>
-    <Header className="header">
-      <div className="logo" />
-      
-        <Menu theme="darkhh" onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="mail" icon={<MailOutlined />}>
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled icon={<AppstoreOutlined />}>
-          Navigation Two
-        </Menu.Item>
-        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-          <Menu.ItemGroup title="Office Note">
-            <Menu.Item key="setting:1">Predefined Office Note(Create)</Menu.Item>
-            <Menu.Item key="setting:2">Generic Office Note(Create)</Menu.Item>
-          </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </Menu.ItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
-      </Menu>
-
-
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
-        
-        <Content style={{ padding: '0 24px', minHeight: 280 }}>Content
-        
-          
-        </Content>
-      </Layout>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-  </Layout>
 
      <Navigation/>
     
